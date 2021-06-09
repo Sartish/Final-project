fc30e469d69311886ad05f74541a20c5ea4617ad
Sara Carlstein <carlstein.sara@gmail.com>
2021-06-09 09:21:17 +0200

    styling for header

--- frontend/src/components/AddedDescription.js
+++ frontend/src/components/AddedDescription.js
@@ -1,29 +1,30 @@
-import React, {useState} from "react"
+import React, { useState } from "react";
 
 import { API_URL } from "../reusables/urls";
 
 const AddedDescription = () => {
-    const [description, setDescription] = useState("")
+  const [description, setDescription] = useState("");
 
+  const onFormSubmit = (e) => {
+    e.preventDefault();
 
-    const onFormSubmit = (e) => {
-        e.preventDefault();
+    const options = {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ description }),
+    };
 
-        const options = {
-          method: "PATCH",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({ description }),
-        };
+    fetch(API_URL("concepts"), options)
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
 
-        fetch(API_URL("concepts"), options)
-        .then((res) => res.json())
-        .then((data) => console.log(data))
-    }
-
-    return(
-        <>
+  return (
+    <>
+      <div>
+        Add your desciption here
         <form onSubmit={onFormSubmit}>
           <input
             type="text"
@@ -31,8 +32,9 @@ const AddedDescription = () => {
             onChange={(e) => setDescription(e.target.value)}
           />
         </form>
-        </>
-    )
+      </div>
+    </>
+  );
 };
 
 export default AddedDescription;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,29 @@
 import React from "react";
-import { Paper, makeStyles } from '@material-ui/core';
+import { Paper, Grid, makeStyles } from "@material-ui/core";
 import Navigation from "../components/Navigation";
 import CustomButton from "../components/CustomButton";
 import space from "../assets/space.jpg";
+import robots from "../assets/robots.png";
 
 const useStyles = makeStyles(() => ({
-  root: {
-    backgroundImage: `url(${space})`,
-    width: 500
+  container: {
+    display: "flex",
+    width: "100vw",
+    height: "100vh",
+    backgroundColor: "blue",
+    alignItems: "center",
+    justifyConter: "space-between",
+    flexDirection: "column",
+  },
+  robots: {
+    display: "flex",
+    backgroundImage: `url(${robots})`,
+    backgroundSize: "cover",
+    alignItems: "center",
+    justifyConter: "center",
+    flexDirection: "column",
+    width: 500,
+    height: 500,
   },
 }));
 
@@ -15,12 +31,18 @@ const Home = () => {
   const classes = useStyles();
   return (
     <div>
-      <Navigation  />
-      <Paper className={classes.root} variant="outlined">
-      <p>Picture</p>
-      <p>icon</p>
-      <CustomButton text="Show me!" />
-      </Paper>
+      <Navigation />
+      <div
+        className={classes.container}
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Paper className={classes.robots} variant="outlined">
+          <p>Picture</p>
+          <p>icon</p>
+          <CustomButton text="Show me!" />
+        </Paper>
+      </div>
     </div>
   );
 };
