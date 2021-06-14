8b8484009e62c61df75ae6aee44fbb7c23942ee0
Sara Carlstein <carlstein.sara@gmail.com>
2021-06-14 08:55:25 +0200

    testing changes up to date

--- frontend/src/pages/Descriptions.js
+++ frontend/src/pages/Descriptions.js
@@ -163,7 +163,7 @@ const useStyles = makeStyles((theme) => ({
   container: {
     backgroundColor: "#e1e8eb",
     width: "100%",
-    height: "1000px",
+    // height: "1000px",
   },
   root: {
     width: "500px",
