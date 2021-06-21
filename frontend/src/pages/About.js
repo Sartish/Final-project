42c3fef218cfb88918619d1bb9fa68d0d61fb3b0
Ann-Sofi Jönsson <annsofi.jonsson@hotmail.se>
2021-06-21 20:39:40 +0200

    styling in about us, also changed text

--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -21,23 +21,25 @@ const About = () => {
               This site was created in Technigo Bootcamp as the final project.
               The idea came from our own experience, entering the tech-world and
               being exposed to A LOT of new words. If you are lucky are they
-              quite self-explanatory for example "something here". But many of
-              them are short for something such as "example", but and a lot of
+              quite self-explanatory for example "code review". But many of
+              them are short for something such as "API", but and a lot of
               word might as well be greek!
             </AboutTheSite>
             <AboutTheSite>
               The purpose of this site is to be a friend in need when there is
-              something you heard somewhere but maybe afraid to ask, or maybe
-              just forgot. And if you feel like you have a good explanation for
-              something, please add it!{" "}
+              something you heard somewhere and are curious about. Or maybe something you just forgot and
+              doesn't want to ask for the 47th time. And if you feel like you have a good explanation for
+              something, be a start and add it! <span role="img" aria-label="star">
+                  {"🌟"}
+                </span>
             </AboutTheSite>
           </InfoText>
 
           <AboutUs>
             <AboutUsCard>
               <Name>Sara Carlstein</Name>
+              <ImgAndLinks>
               <SaraImg src={saraimg} alt="Sara Carlstein" />
-              <AboutUsText>Text and link to likedin and github</AboutUsText>
               <LinksDiv>
                 <Links href="https://www.linkedin.com/in/sara-carlstein-532b2737/">
                   LinkedIn
@@ -46,11 +48,12 @@ const About = () => {
                   Portfolio
                 </Links>
               </LinksDiv>
+              </ImgAndLinks>
             </AboutUsCard>
             <AboutUsCard>
-              <Name>Ann-Sofi Jönsson </Name>
+              <Name2>Ann-Sofi Jönsson</Name2>
+              <ImgAndLinks>
               <AnnSofiImg src={annsofiimg} alt="Ann-Sofi Jönsson" />
-              <AboutUsText>Text and link to likedin and github</AboutUsText>
               <LinksDiv>
                 <Links href="https://www.linkedin.com/in/annsofijonsson/">
                   LinkedIn
@@ -59,6 +62,7 @@ const About = () => {
                   Portfolio
                 </Links>
               </LinksDiv>
+              </ImgAndLinks>
             </AboutUsCard>
           </AboutUs>
         </Wrapper>
@@ -76,65 +80,13 @@ const LightPink = "#FFCFF1";
 const Gray = "#404040;";
 const DarkGray = "#282828";
 
-const AboutTheSite = styled.p`
-  font-size: 16px;
-  margin-left: 5px;
-  margin-right: 5px;
-  font-family${ParagraphFont};
-  color: ${Gray};
-  line-height: 130%;
-`;
-
-const SaraImg = styled.img`
-  height: 150px;
-  border-radius: 10%;
-  margin-left: 10px;
-`;
-
-const AnnSofiImg = styled.img`
-  height: 150px;
-  border-radius: 10%;
-  margin-left: 10px;
-`;
-const LinksDiv = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  margin-bottom: 10px;
-`;
-
-const AboutUsCard = styled.div`
-  border: 4px solid ${LightPink};
-  margin-bottom: 20px;
-`;
-
-const AboutUsText = styled.p`
-  font-size: 16px;
-  margin-left: 10px;
-  font-family${ParagraphFont},
-  color: ${Gray}
-`;
-
-const Name = styled.h1`
-  font-family: ${ParagraphFont};
-  color: ${Gray};
-  font-size: 20px;
-  margin-left: 10px;
-`;
-
-const Links = styled.a`
-  text-decoration: none;
-  color: black;
-  cursor: pointer;
-  font-family: ${ParagraphFont};
-  color: ${Gray};
-`;
-
 const Container = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 `;
+
 const Wrapper = styled.div`
   max-width: 350px;
   margin-top: 20px;
@@ -146,52 +98,24 @@ const Wrapper = styled.div`
   }
 `;
 
-const ContainerHeader = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: space-around;
-  border: 2px black;
-  padding: 20px;
-
-  @media (min-width: 768px) {
-    justify-content: space-around;
-    flex-direction: row;
-    width: 1200px;
-    border: black solid 2px;
-    box-sizing: border-box;
-    padding: 0;
-  }
-`;
-
 const InfoText = styled.div`
   // border: 2px solid pink;
 `;
 
-const Title = styled.h3`
+const Title = styled.h1`
+  font-family: ${HeaderFont}
   display: flex;
   flex-direction: column;
   justify-content: center;
-  align-items: center;
+  // align-items: center;
+  text-align: center;
   font-size: 24px;
   padding: 10px;
   margin: 0px;
   font-family: ${HeaderFont};
   color: ${DarkGray};
   @media (min-width: 768px) {
-    font-zize: 35px;
-  }
-`;
-
-const AboutUs = styled.div`
-  display: flex;
-  flex-direction: column;
-  border: 2px solid ${LightPink};
-  margin-top: 20px;
-
-  @media (min-width: 768px) {
-    // justify-content: space-around;
-    // flex-direction: row;
+    font-zize: 40px;
   }
 `;
 
@@ -203,7 +127,122 @@ const Border = styled.div`
   border:  #ffcff1; 2px solid;
   background-color: #ffcff1;
   border-radius: 50px;
+
   @media (min-width: 768px) {
     width: 800px;
   }
 `;
+
+const AboutTheSite = styled.p`
+  font-size: 16px;
+  margin-left: 5px;
+  margin-right: 5px;
+  font-family${ParagraphFont};
+  color: ${Gray};
+  line-height: 130%;
+`;
+
+const AboutUs = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+
+  @media (min-width: 768px) {
+    justify-content: space-evenly;
+    flex-direction: row;
+  }
+`;
+
+const AboutUsCard = styled.div`
+  border: 4px solid ${LightPink};
+  margin-bottom: 20px;
+  border-radius: 5px;
+
+  @media (min-width: 768px) {
+    width: 350px;
+  }
+`;
+
+const Name = styled.h1`
+  font-family: ${ParagraphFont};
+  color: ${Gray};
+  font-size: 20px;
+  margin-bottom: 15px;
+  text-align: center;
+`;
+
+const Name2 = styled.h1`
+  font-family: ${ParagraphFont};
+  color: ${Gray};
+  font-size: 20px;
+  margin-bottom: 15px;
+  text-align: center;
+
+    @media (min-width: 768px) {
+    margin-bottom: 7px;
+  }
+`;
+
+const ImgAndLinks = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 15px;
+  margin-left: 15px;
+  margin-right: 20px;
+`;
+
+const LinksDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+  margin-right: 25px;
+
+  @media (min-width: 768px) {
+    margin-right: 35px;
+  }
+`;
+
+const Links = styled.a`
+  text-decoration: none;
+  color: black;
+  cursor: pointer;
+  background-color: ${LightPink};
+  border-radius: 10px;
+  padding: 7px;
+  font-family: ${ParagraphFont};
+  color: ${Gray};
+  :hover {
+    color: ${HotPink} ;
+  }
+`;
+
+const SaraImg = styled.img`
+  height: 150px;
+  width: 142px;
+  border-radius: 20%;
+  margin-left: 10px;
+  text-align: center;
+`;
+
+const AnnSofiImg = styled.img`
+  height: 160px;
+  width: 120px;
+  border-radius: 20%;
+  margin-left: 10px;
+`;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
